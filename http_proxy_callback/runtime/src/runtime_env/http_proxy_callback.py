dfc59dca6b41f564dd971580ba8183b89c252442
hoshyari <hoshyari@adobe.com>
2023-06-07 06:17:18 +0000

    works

--- http_proxy_callback/runtime/src/runtime_env/http_proxy_callback.py
+++ http_proxy_callback/runtime/src/runtime_env/http_proxy_callback.py
@@ -1,48 +1,28 @@
-import functools
 import logging
 import os
-import re
-import typing
 
 from runtime_env.pydev import PYDEV_CONFIG_KEY, PydevConfig, connect_to_pydev
-from starlette.datastructures import Headers, MutableHeaders
 from starlette.middleware import Middleware
-from starlette.responses import PlainTextResponse, Response
-from starlette.types import ASGIApp, Message, Receive, Scope, Send
+from starlette.middleware.base import (BaseHTTPMiddleware,
+                                       RequestResponseEndpoint)
+from starlette.requests import Request
+from starlette.responses import Response
 
 logger = logging.getLogger("ray.serve")
 
 
-class RequestIDMiddleware:
-    #ADAPTED FROM: https://github.com/encode/starlette/blob/master/starlette/middleware/cors.py
-    def __init__(self, app: ASGIApp) -> None:
-        self.app = app
+class RequestIdMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        headers = dict(request.scope['headers'])
+        headers[b'x-request-id'] = b'123'
+        request.scope['headers'] = [(k, v) for k, v in headers.items()]
 
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        await self.simple_response(scope, receive, send)
-
-    async def simple_response(
-        self, scope: Scope, receive: Receive, send: Send, request_headers: Headers
-    ) -> None:
-        send = functools.partial(self.send, send=send)
-        await self.app(scope, receive, send)
-
-    async def send(
-        self, message: Message, send: Send
-    ) -> None:
-        if message["type"] != "http.response.start":
-            await send(message)
-            return
-
-        message.setdefault("headers", [])
-        headers = MutableHeaders(scope=message)
-        headers.update({"x-request-id": "goooooz"})
-
-        await send(message)
+        response = await call_next(request)
+        return response
 
 
 def proxy_callback() -> list[Middleware]:
     pydev_config = os.getenv(PYDEV_CONFIG_KEY)
     if pydev_config is not None:
         connect_to_pydev(PydevConfig.parse_raw(pydev_config))
-    return [Middleware(RequestIDMiddleware)]
+    return [Middleware(RequestIdMiddleware)]
